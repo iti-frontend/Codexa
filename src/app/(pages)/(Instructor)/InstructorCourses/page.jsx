1f8ec421a585530f07676ffcd7b1c4d2af1b6d91
abanoub-abdelmessih <abanoubabdelmessih110@gmail.com>
2025-10-15 23:41:42 +0300

    add instructor courses

---
+++ src/app/(pages)/(Instructor)/InstructorCourses/page.jsx
@@ -0,0 +1,120 @@
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Plus } from "lucide-react";
+import Image from "next/image";
+
+const tabs = [
+  {
+    name: "Active",
+    value: "active",
+    content: (
+      <>
+        <CourseCard
+          title="JavaScript"
+          desc="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis facere, dolores aliquam non voluptate vero pariatur tempora temporibus dolore, consectetur reprehenderit nostrum in impedit ut voluptates aut molestiae molestias sunt."
+        />
+        <CourseCard
+          title="React"
+          desc="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis facere, dolores aliquam non voluptate vero pariatur tempora temporibus dolore, consectetur reprehenderit nostrum in impedit ut voluptates aut molestiae molestias sunt."
+        />
+      </>
+    ),
+  },
+  {
+    name: "Drafts",
+    value: "drafts",
+    content: (
+      <>
+        <CourseCard
+          title="Node.js"
+          desc="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis facere, dolores aliquam non voluptate vero pariatur tempora temporibus dolore, consectetur reprehenderit nostrum in impedit ut voluptates aut molestiae molestias sunt."
+        />
+      </>
+    ),
+  },
+  {
+    name: "Archived",
+    value: "archived",
+    content: (
+      <>
+        <CourseCard
+          title="TypeScript"
+          desc="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis facere, dolores aliquam non voluptate vero pariatur tempora temporibus dolore, consectetur reprehenderit nostrum in impedit ut voluptates aut molestiae molestias sunt."
+        />
+      </>
+    ),
+  },
+];
+
+function InstructorCourses() {
+  return (
+    <>
+      {/* Courses Header */}
+      <header className="flex items-start justify-between p-5">
+        <div>
+          <h1 className="text-xl lg:text-3xl font-bold mb-1">My Courses</h1>
+          <h5 className="text-muted-foreground text-xs md:text-base">
+            Manage and organize your courses
+          </h5>
+        </div>
+        <Button className="text-xs md:text-base">
+          <Plus /> New Course
+        </Button>
+      </header>
+
+      {/* Tab List */}
+      <Tabs defaultValue="active" className="gap-4 p-0">
+        <div className="border-b px-5">
+          <TabsList className="bg-background rounded-none p-0 space-x-5">
+            {tabs.map((tab) => (
+              <TabsTrigger
+                key={tab.value}
+                value={tab.value}
+                className="data-[state=active]:dark:bg-background data-[state=active]:text-primary data-[state=active]:dark:text-primary data-[state=active]:border-primary p-0 dark:data-[state=active]:border-primary h-full rounded-none border-0 border-b-2 border-transparent data-[state=active]:shadow-none"
+              >
+                {tab.name}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+        </div>
+
+        {/* Tab Content */}
+        {tabs.map((tab) => (
+          <TabsContent
+            key={tab.value}
+            value={tab.value}
+            className="space-y-4 px-3 pb-3 md:px-5"
+          >
+            {tab.content}
+          </TabsContent>
+        ))}
+      </Tabs>
+    </>
+  );
+}
+export default InstructorCourses;
+
+function CourseCard({ title, desc }) {
+  return (
+    <div className="bg-sidebar p-3 rounded-3xl border border-border flex flex-col md:flex-row gap-4">
+      {/* Course Image */}
+      <div className="relative w-full md:w-64 md:h-36 lg:w-72 xl:w-80 h-40 shrink-0">
+        <Image
+          src={"/auth/login.png"}
+          alt=""
+          fill
+          className="rounded-md object-cover"
+        />
+      </div>
+
+      {/* Course Details */}
+      <div className="flex flex-col gap-4 md:gap-0 justify-between max-w-lg">
+        <div className="space-y-2">
+          <h4 className="font-bold text-lg md:text-2xl">{title}</h4>
+          <p className="text-foreground/70 text-sm line-clamp-2">{desc}</p>
+        </div>
+        <Button className={"w-fit"}>Manage Course</Button>
+      </div>
+    </div>
+  );
+}
--- src/app/(pages)/layout.jsx
+++ src/app/(pages)/layout.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import React from "react";
 import SidebarComponent from "@/components/shared/SidebarComponent";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { InstructorLinks, StudentsLinks } from "@/Constants/sidebar-links";
-function PagesLayout({children}) {
-    const role ='instructor' 
+function PagesLayout({ children }) {
+  const role = "instructor";
   return (
-        <SidebarProvider>
+    <SidebarProvider>
       {/* The Main Side bar */}
-      <SidebarComponent Links={ role==='instructor' ? InstructorLinks : StudentsLinks}  />
+      <SidebarComponent
+        Links={role === "instructor" ? InstructorLinks : StudentsLinks}
+      />
 
       {/* The Main Content */}
-      <main className="bg-background w-screen border rounded-xl m-5 p-5">
-        <SidebarTrigger />
+      <main className="bg-background w-screen border rounded-2xl m-3 md:m-5 shadow-md">
+        {/* Main Header */}
+        <header className="flex gap-2 border-b p-2">
+          <SidebarTrigger />
+          <h1 className="font-bold text-xl text-primary border-l pl-2">
+            Codexa
+          </h1>
+        </header>
+
+        {/* Main Content */}
         {children}
       </main>
     </SidebarProvider>
-  )
+  );
 }
 
 export default PagesLayout;
