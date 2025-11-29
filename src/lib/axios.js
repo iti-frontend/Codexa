ce05aff0b2c03fdd0522add6753bb92a2e586218
mustafa-muhamma <shakhsiamohamedabelmaguidgttzv@gmail.com>
2025-11-29 15:46:54 +0200

    Fix baseURL environment variable in axios configuration

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000/api",
   timeout: 10000,
 });
 
