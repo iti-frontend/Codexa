ff9e07f78f72ad76f55267c9903a442f902d3752
Abanoub Abdelmessih <Abanoubabdelmessih110@gmail.com>
2025-12-06 22:02:18 +0200

    Delete .history/src directory

--- .history/src/app/(pages)/student/profile/editProfile/page_20251114101804.jsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import ProfileEditCard from "@/components/Dashboard/ProfileEditCard";
-
-export default function StudentEdit() {
-  return <ProfileEditCard editEndpoint={`${process.env.NEXT_PUBLIC_API_BASE_URL}/Students/profile`} />;
-}
